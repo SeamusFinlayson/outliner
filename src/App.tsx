6175e5f65d8fa62a1faf267b25e668658c86127f
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2024-01-26 19:01:55 -0800

    Restrict action to users with GM role

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,17 @@ export function App() {
 
   const sceneReady = useOwlbearStore((state) => state.sceneReady);
 
-  if (sceneReady) {
-    return <Outliner />;
-  } else {
+  const role = useOwlbearStore((state) => state.role);
+
+  if (!sceneReady) {
     return (
       <Header title="Outliner" subtitle="Open a scene to use the outliner" />
     );
+  } else if (role !== "GM") {
+    return (
+      <Header title="Outliner" subtitle="Must have GM access to use (this fork of) outliner." />
+    );
+  } else {
+    return <Outliner />;
   }
 }
